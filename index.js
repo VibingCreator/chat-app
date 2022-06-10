18f117156e099e3386ae41cef6a51228f1fa0a04
VibingCreator <VibingCreator.Workspace@gmail.com>
2022-06-10 21:14:47 +0200

    integrate socket.io with http server and static website

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
+import { createServer } from "http";
 import express from "express";
+import { Server } from "socket.io";
 
 const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer);
 
 const host = process.env.HOST || "127.0.0.1";
 const port = process.env.PORT || 3000;
 
 app.use("/", express.static("public"));
 
-app.listen(port, host, () => {
+io.on("connection", (socket) => {
+    console.log("connection established");
+
+    socket.on("disconnect", (reason) => {
+        console.log(`connection closed (${reason})`);
+    })
+});
+
+httpServer.listen(port, host, () => {
     console.log(`http://${host}:${port}`);
 });
